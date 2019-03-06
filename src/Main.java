5ee1fee27040598d85cd54c3ac98336de3f980ff
AliLordLoss <a.a.ghanati@gmail.com>
2019-03-06 17:10:19 +0330

    commited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("fuck worid");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int x = 51;
+        int y = 43;
+        System.out.println(x * y + 47);
     }
 }
